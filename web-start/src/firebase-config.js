aca4e38e1aa8a6e8e7aca07fba967966611c72a0
karelgortz <karel.gortz@student.odisee.be>
2022-02-15 23:18:31 +0100

    15/02

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -18,3 +18,22 @@ export function getFirebaseConfig() {
     return config;
   }
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAw1fgBH7DM5fuYIvI4C_DgmHXeOElho3g",
+  authDomain: "friendlychat-10aa1.firebaseapp.com",
+  projectId: "friendlychat-10aa1",
+  storageBucket: "friendlychat-10aa1.appspot.com",
+  messagingSenderId: "1006632411303",
+  appId: "1:1006632411303:web:173f4ad3d26ad5c1123b82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
